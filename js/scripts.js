014bb22897c5d1108a1f9a34c0beb15098293530
Jonathan Cheng <jcheng461@gmail.com>
2022-03-13 02:18:12 +0900

    WIP: add function to take text passage as parameter, and return the most common words to html page

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,26 @@ function wordCounter(text) {
   return wordCount;
 }
 
+// Function that bolds text the user is searching for.
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function (element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== textArray.length - 1) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
+
 // Function that counts how many times a word appears in a given text.
 function numberOfOccurrencesInText(word, text) {
   if (noInputtedWord(word, text)) {
@@ -36,26 +56,32 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
-// Function that bolds text the user is searching for.
-function boldPassage(word, text) {
-  if (noInputtedWord(word, text)) {
-    return "";
-  }
-  let htmlString = "<p>";
-  let textArray = text.split(" ");
-  console.log("Words in text split into array: " + textArray);
-  textArray.forEach(function (element, index) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      htmlString = htmlString.concat("<b>" + element + "</b>");
-    } else {
-      htmlString = htmlString.concat(element);
-    }
-    if (index !== textArray.length - 1) {
-      htmlString = htmlString.concat(" ");
-    }
+// Function that returns the most common words in a passage.
+function mostCommon(text) {
+  const textArray = text.split(" ");
+  const elementCount = [];
+
+  textArray.forEach(function (element) {
+    elementCount.push(numberOfOccurrencesInText(element, text));
   });
-  console.log(htmlString + "</p>");
-  return htmlString + "</p>";
+  console.log("Text in ea. position: " + textArray);
+  console.log("# of each element: " + elementCount);
+  // Loop through the text array positions, and then zero the elementCount array at the position.
+  // Hi there hey yo hi hi yay yo whoa there whoa... yay!
+  const uniqueWords = [];
+  textArray.forEach(function (element) {
+    // Hi,there,hey,yo,hi,hi,yay,yo,whoa,there,whoa...,yay!
+    // 3,     2,  1, 2, 3, 3,  2, 2,   2,    2,   1,     1
+
+    if (!uniqueWords.includes(element.toLowerCase())) {
+      uniqueWords.push(element.toLowerCase());
+    }
+    // First, it goes to "Hi". This word has not been logged yet. Great.
+    // Then it checks "there". This word has not been logged yet. Great.
+    // ..
+    // Then it sees "hi". This word has already been logged. Therefore zero the other array at this position --- elementCount[index] = 0;
+  });
+  console.log("Array of unique words: " + uniqueWords);
 }
 
 // User Interface Logic
@@ -70,7 +96,7 @@ $(document).ready(function () {
 
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
-
     $("#bolded-passage").html(boldPassage(word, passage));
+    $("#most-common").html(mostCommon(passage));
   });
 });
